1964a1214fbd6224f813201f4519396f8e0b0014
Josh Darby <josh@inn.org>
2019-11-21 16:12:55 -0500

    Base Grunt/LESS/css/enqueuing and updated gitignore

---
+++ wp-content/themes/caribbean/inc/enqueue.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Enqueue specific styles and scripts for Caribbean child theme
+ */
+function caribbean_enqueue_styles(){
+	wp_enqueue_style(
+		'typekit',
+		'https://use.typekit.net/xkz6lbv.css'
+	);
+	wp_enqueue_style(
+		'largo-child-styles',
+		get_stylesheet_directory_uri() . '/css/child-style.css',
+		array( 'largo-stylesheet', 'typekit' ),
+		filemtime( get_stylesheet_directory() . '/css/child-style.css' )
+	);
+}
+add_action( 'wp_enqueue_scripts', 'caribbean_enqueue_styles' );
