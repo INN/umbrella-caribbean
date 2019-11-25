50ad2212405bdfde5abc4b18a123956a69affc6b
Josh Darby <josh@inn.org>
2019-11-25 10:09:37 -0500

    Use largo_byline function name instead, also extend Largo_Byline

--- wp-content/themes/caribbean/functions.php
+++ wp-content/themes/caribbean/functions.php
@@ -43,54 +43,54 @@ add_action( 'after_setup_theme', 'largo_child_require_files' );
  * @return String Byline as formatted html
  * @since 0.1
  */
-if ( ! function_exists( 'caribbean_byline' ) ) {
-	function caribbean_byline( $echo = true, $exclude_date = false, $post = null ) {
-		// Get the post ID
-		if (!empty($post)) {
-			if (is_object($post))
-				$post_id = $post->ID;
-			else if (is_numeric($post))
-				$post_id = $post;
-		} else {
-			$post_id = get_the_ID();
-			if ( WP_DEBUG || LARGO_DEBUG ) {
-				_doing_it_wrong( 'largo_byline', 'largo_byline must be called with a post or post ID specified as the third argument. For more information, see https://github.com/INN/largo/issues/1517 .', '0.6' );
-			}
+function largo_byline( $echo = true, $exclude_date = false, $post = null, $show_avatar = true ) {
+	// Get the post ID
+	if (!empty($post)) {
+		if (is_object($post))
+			$post_id = $post->ID;
+		else if (is_numeric($post))
+			$post_id = $post;
+	} else {
+		$post_id = get_the_ID();
+		if ( WP_DEBUG || LARGO_DEBUG ) {
+			_doing_it_wrong( 'largo_byline', 'largo_byline must be called with a post or post ID specified as the third argument. For more information, see https://github.com/INN/largo/issues/1517 .', '0.6' );
 		}
-		// Set us up the options
-		// This is an array of things to allow us to easily add options in the future
-		$options = array(
-			'post_id' => $post_id,
-			'values' => get_post_custom( $post_id ),
-			'exclude_date' => $exclude_date,
-		);
-		if ( function_exists( 'get_coauthors' ) ) {
-			// If Co-Authors Plus is enabled and there is not a custom byline
-			$byline = new Largo_CoAuthors_Byline( $options );
-		} else {
-			// no custom byline, no coauthors: let's do the default
-			$byline = new Caribbean_Byline( $options );
-		}
-		/**
-		 * Filter the largo_byline output text to allow adding items at the beginning or the end of the text.
-		 *
-		 * @since 0.5.4
-		 * @param string $partial The HTML of the output of largo_byline(), before the edit link is added.
-		 * @param array $array Associative array of argument name => argument value, with the arguments passed to largo_byline(). Since https://github.com/INN/largo/issues/1656
-		 * @link https://github.com/INN/Largo/issues/1070
-		 */
-		$byline = apply_filters(
-			'largo_byline',
-			$byline,
-			array(
-				'echo' => $echo,
-				'exclude_date' => $exclude_date,
-				'post' => $post
-			)
-		);
-		if ( $echo ) {
-			echo $byline;
-		}
-		return $byline;
 	}
+	// Set us up the options
+	// This is an array of things to allow us to easily add options in the future
+	$options = array(
+		'post_id' => $post_id,
+		'values' => get_post_custom( $post_id ),
+		'exclude_date' => $exclude_date,
+		'show_avatar' => $show_avatar,
+		
+	);
+	if ( function_exists( 'get_coauthors' ) ) {
+		// If Co-Authors Plus is enabled and there is not a custom byline
+		$byline = new Largo_CoAuthors_Byline( $options );
+	} else {
+		// no custom byline, no coauthors: let's do the default
+		$byline = new Caribbean_Byline( $options );
+	}
+	/**
+	 * Filter the largo_byline output text to allow adding items at the beginning or the end of the text.
+	 *
+	 * @since 0.5.4
+	 * @param string $partial The HTML of the output of largo_byline(), before the edit link is added.
+	 * @param array $array Associative array of argument name => argument value, with the arguments passed to largo_byline(). Since https://github.com/INN/largo/issues/1656
+	 * @link https://github.com/INN/Largo/issues/1070
+	 */
+	$byline = apply_filters(
+		'largo_byline',
+		$byline,
+		array(
+			'echo' => $echo,
+			'exclude_date' => $exclude_date,
+			'post' => $post
+		)
+	);
+	if ( $echo ) {
+		echo $byline;
+	}
+	return $byline;
 }
--- wp-content/themes/caribbean/inc/byline_class.php
+++ wp-content/themes/caribbean/inc/byline_class.php
@@ -1,190 +1,44 @@
 <?php
-/**
- * This file contains the Caribbean_Byline class, and it's based off of Largo_Byline
- * 
- * Modifications:
- * - Adds a wrapper div around the byline avatar
- * - Removes conditional that only displayed avatar on single posts
- */
 
-/**
- * Generates a byline for a normal WordPress user
- *
- * @param Array $args an array with the following keys:
- *     $args = [
- *         'post_id' => (int) the ID of the post that we are creating a byline for
- *         'exclude_date ' => (bool) Whether or not to display the date
- *     ]
- */
-class Caribbean_Byline {
+// require the main Largo_Byline class so we can extend it
+require_once( get_template_directory().'/inc/byline_class.php' );
 
-	/** @var int The ID of the post this byline is for */
-	protected $post_id;
+// For Largo Custom Bylines
+class Caribbean_Byline extends Largo_Byline {
 
-	/** @var bool Whether or not the byline should include the date */
-	private $exclude_date;
-
-	/**
-	 * @var array The post's custom fields
-	 * @link https://codex.wordpress.org/Function_Reference/get_post_custom
-	 */
-	private $custom;
-
-	/**
-	 * Temporary variable used for the author ID;
-	 * This must be public, because Largo_CoAuthors_Byline's methods incorporate methods from Largo_Byline, and parent classes cannot see private or protected members of extending classes.
-	 * @var int The ID of the author for this post
-	 */
-	public $author_id;
-
-	/**
-	 * @var string The HTML ouput of this class
-	 * @see __toString
-	 */
-	public $output;
-
-	function __construct( $args ) {
-		$this->populate_variables( $args );
-		$this->generate_byline();
-	}
-
-	/**
-	 * Set us up the vars
-	 *
-	 * @param array $args Associative array containing following keys:
-	 *     - 'post_id': an integer post ID
-	 *     - 'exclude_date': boolean whether or not to include the date in the byline
-	 *
-	 * @see $post_id      Sets this from $args
-	 * @see $exclude_date Sets this from $args
-	 * @see $custom       Fills this array with the output of get_post_custom
-	 * @see $author_id    Sets this from the post meta
-	 */
 	function populate_variables( $args ) {
 		$this->post_id = $args['post_id'];
 		$this->exclude_date = $args['exclude_date'];
 		$this->custom = get_post_custom( $this->post_id );
 		$this->author_id = get_post_meta( $this->post_id, 'post_author', true );
+		$this->show_avatar = $args['show_avatar'];
 	}
 
 	/**
-	 * this creates the byline text and adds it to $this->output
-	 *
-	 * @see $output Creates this
-	 */
-	function generate_byline() {
-		ob_start();
-
-		// Author-specific portion of byline
-		$this->avatar();
-		$this->author_link();
-		$this->job_title();
-		$this->twitter();
-
-		// The generic parts
-		$this->maybe_published_date();
-		$this->edit_link();
-
-		$this->output = ob_get_clean();
-	}
-
-	/**
-	 * This is what turns the whole class into a string
-	 *
-	 * @see $output
-	 * @see generate_byline()
-	 */
-	public function __toString() {
-		return $this->output;
-	}
-
-	/**
-	 * On single posts, output the avatar for the author object
-	 * This supports both Largo_Byline and Largo_CoAuthors_Byline
+	 * Modifies the default Largo_Byline avatar function to wrap
+	 * the avatar in a div so we can style the avatar with a specific inner outline
 	 */
 	function avatar() {
-        $output = '';
-        $author_email = get_the_author_meta( 'email', $this->author_id );
-        if ( isset( $this->author ) && $this->author->type == 'guest-author' && get_the_post_thumbnail( $this->author->ID ) ) {
-            $output = get_the_post_thumbnail( $this->author->ID, array( 60,60 ) );
-            $output = str_replace( 'attachment-32x32', 'avatar avatar-32 photo', $output );
-            $output = str_replace( 'wp-post-image', '', $output );
-        } else if ( largo_has_avatar( $author_email ) ) {
-            $output = get_avatar(
-                $author_email,
-                32,
-                '',
-                get_the_author_meta( 'display_name', $this->author_id ),
-                array(
-                    'class' => 'avatar avatar-32 photo',
-                )
-            );
-        }
-        $output .= ' '; // to reduce run-together bylines
-		echo '<div class="byline-avatar-wrapper inner-circle">'.$output.'</div>';
-	}
-
-	/**
-	 * a wrapper around largo_author_link
-	 */
-	function author_link() {
-		$authors = largo_author_link( false, $this->post_id );
-		$output = '<span class="by-author"><span class="by">' . __( 'By', 'largo' ) . '</span> <span class="author vcard" itemprop="author">' . $authors . '</span></span>';
-		echo $output;
-	}
-
-	/**
-	 * If job titles are enabled by Largo's theme option, display the one for this author
-	 */
-	function job_title() {
-		$show_job_titles = of_get_option( 'show_job_titles', false );
 		$output = '';
-		// only do this if we're showing job titles and there is one to be shown
-		if ( $show_job_titles && $job = get_the_author_meta( 'job_title' , $this->author_id ) ) {
-			$output .= '<span class="job-title"><span class="comma">,</span> ' . $job . '</span>';
-		}
-		echo $output;
-	}
-
-	/**
-	 * If this author has a twitter ID, output it as a link on an i.icon-twitter
-	 */
-	function twitter() {
-		$twitter = get_the_author_meta( 'twitter', $this->author_id );
-		$output = '';
-		if ( $twitter && is_single() ) {
-			$output .= ' <span class="twitter"><a href="https://twitter.com/' . largo_twitter_url_to_username( $twitter ) . '"><i class="icon-twitter"></i></a></span>';
-		}
-		echo $output;
-	}
-
-	/**
-	 * Determine whether to display the date
-	 */
-	function maybe_published_date() {
-		if ( ! $this->exclude_date ) {
-			$this->published_date();
-		}
-	}
-
-	/**
-	 * A wrapper around largo_time to determine when the post was published
-	 */
-	function published_date() {
-		echo sprintf(
-			'<span class="sep"> |</span> <time class="entry-date updated dtstamp pubdate" datetime="%1$s">%2$s</time>',
-			esc_attr( get_the_date( 'c', $this->post_id ) ),
-			largo_time( false, $this->post_id )
-		);
-	}
-
-	/**
-	 * Output the edit link for this post, only to admin users
-	 */
-	function edit_link() {
-		// Add the edit link if the current user can edit the post
-		if ( current_user_can( 'edit_post', $this->post_id ) ) {
-			echo ' <span class="edit-link"><a href="' . get_edit_post_link( $this->post_id ) . '">' . __( 'Edit This Post', 'largo' ) . '</a></span>';
+		if( true === $this->show_avatar ){
+			$author_email = get_the_author_meta( 'email', $this->author_id );
+			if ( isset( $this->author ) && $this->author->type == 'guest-author' && get_the_post_thumbnail( $this->author->ID ) ) {
+				$output = get_the_post_thumbnail( $this->author->ID, array( 60,60 ) );
+				$output = str_replace( 'attachment-32x32', 'avatar avatar-32 photo', $output );
+				$output = str_replace( 'wp-post-image', '', $output );
+			} else if ( largo_has_avatar( $author_email ) ) {
+				$output = get_avatar(
+					$author_email,
+					32,
+					'',
+					get_the_author_meta( 'display_name', $this->author_id ),
+					array(
+						'class' => 'avatar avatar-32 photo',
+					)
+				);
+			}
+			$output .= ' '; // to reduce run-together bylines
+			echo '<div class="byline-avatar-wrapper inner-circle">'.$output.'</div>';
 		}
 	}
 }
